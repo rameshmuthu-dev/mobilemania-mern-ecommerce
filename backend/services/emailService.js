d746a553b1883384554db629d55dbafffdd3e80e
unknown <you@example.com>
2025-10-23 09:36:18 +0530

    Update: Frontend and backend code change

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -10,8 +10,7 @@ const sendEmail = asyncHandler(async (email, subject, text, attachments = []) =>
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
         },
-        connectionTimeout: 10000,
-        socketTimeout: 10000,
+       
     });
 
     const mailOptions = {
