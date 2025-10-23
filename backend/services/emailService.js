4bfc84bc37cf5dc18241385920efe9760a7c2bab
unknown <you@example.com>
2025-10-23 08:43:22 +0530

    Update: Frontend and backend code changes

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 const sendEmail = asyncHandler(async (email, subject, text, attachments = []) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
-        port: 465,
-        secure: true,
+        
+        
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
